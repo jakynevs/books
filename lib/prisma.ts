987f89faa626c48373e880064373de52332f86b8
jakynevs <johnnevilleis@hotmail.co.uk>
2023-10-09 11:20:20 +0200

    package updates to work on mac

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+let prisma = new PrismaClient();
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
