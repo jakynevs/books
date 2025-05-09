8afa1647d13c4ea42ce9d29f881ca9b06a8416c1
jackNevilleNWorld <john.neville@n.world>
2025-05-09 20:16:34 +0200

    Added globalstyles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
-import "../components/styles/global";
+import globalStyles from "../components/styles/global";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider>
+      <style jsx global>
+        {globalStyles}
+      </style>
       <Component {...pageProps} />
     </SessionProvider>
   );
